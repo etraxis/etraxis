5ceaef7e7c18d12a2999ad490c0d9cd00a761cd5
Artem Rodygin <arodygin@users.noreply.github.com>
2010-02-15 19:22:11 +1000

    Issue 26: Show record assignments on record detail page

--- src/sql/records/elist2.sql
+++ src/sql/records/elist2.sql
@@ -4,10 +4,12 @@ select
     e.originator_id,
     e.event_type,
     e.event_time,
+    e.event_param,
     a.username,
     a.fullname,
     s.state_id,
-    s.state_name
+    s.state_name,
+    s.responsible
 
 from
 
@@ -30,10 +32,12 @@ select
     e.originator_id,
     e.event_type,
     e.event_time,
+    e.event_param,
     a.username,
     a.fullname,
     null as state_id,
-    null as state_name
+    null as state_name,
+    null as responsible
 
 from
 
@@ -44,7 +48,7 @@ where
 
     a.account_id  = e.originator_id and
     e.record_id   = %1              and
-    (e.event_type = 7 or e.event_type = 13)
+    (e.event_type = 2 or e.event_type = 7 or e.event_type = 13)
 
 order by
 
