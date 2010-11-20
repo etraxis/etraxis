a98dde5ababcc164840bc28ea192ca2a3542d39f
arodygin <arodygin@localhost>
2010-11-20 16:17:37 +1000

    Issue 148: New Install of eTraxis on IIS + MsSQL DB Script error

--- tools/sql/mssql2k.sql
+++ tools/sql/mssql2k.sql
@@ -1215,7 +1215,8 @@ insert into tbl_accounts
     csv_delim,
     csv_encoding,
     csv_line_ends,
-    view_id
+    view_id,
+    theme_name
 )
 values
 (
@@ -1224,5 +1225,5 @@ values
     'root@example.com',
     'd41d8cd98f00b204e9800998ecf8427e',
     'Built-in administrator',
-    NULL, 0, 0, 1, 0, 0, 0, 0, 1000, 20, 10, 44, 1, 1, NULL
+    NULL, 0, 0, 1, 0, 0, 0, 0, 1000, 20, 10, 44, 1, 1, NULL, 'Emerald'
 );
