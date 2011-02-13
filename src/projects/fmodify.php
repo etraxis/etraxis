babb872789cd48b50f9bc072f66fb9b050250386
Artem Rodygin <arodygin@users.noreply.github.com>
2011-02-13 22:12:48 +1000

    Issue 211: Automatically updating views

--- src/projects/fmodify.php
+++ src/projects/fmodify.php
@@ -3,7 +3,7 @@
 //------------------------------------------------------------------------------
 //
 //  eTraxis - Records tracking web-based system
-//  Copyright (C) 2005-2010  Artem Rodygin
+//  Copyright (C) 2005-2011  Artem Rodygin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -195,6 +195,8 @@ if (try_request('submitted') == 'modifyform')
 
         $error = field_modify($id,
                               $field['state_id'],
+                              $field['state_name'],
+                              $field['field_name'],
                               $field_name,
                               $field['field_order'],
                               $field_order,
---
+++ src/sql/fields/views.sql
@@ -0,0 +1,3 @@
+update tbl_view_columns
+set field_name = '%3'
+where state_name = '%1' and field_name = '%2'
