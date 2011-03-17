530a72edf7a0dec1cb8c6be6e57e6dde36048732
Artem Rodygin <arodygin@users.noreply.github.com>
2011-03-17 23:38:04 +1000

    Issue 253: Error when displaying Metrics Page

--- src/projects/metrics.php
+++ src/projects/metrics.php
@@ -3,7 +3,7 @@
 //------------------------------------------------------------------------------
 //
 //  eTraxis - Records tracking web-based system
-//  Copyright (C) 2005-2010  Artem Rodygin
+//  Copyright (C) 2005-2011  Artem Rodygin
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -154,6 +154,8 @@ for ($i = $first_week; $i <= $last_week; $i++)
 }
 
 $xml .= <<<jqPlot
+
+    $('#chart').css('height', 0);   // workaround for excanvas bug in IE
 
     chart = $.jqplot('chart', [dataOpened, dataCreated, dataClosed], {
 
