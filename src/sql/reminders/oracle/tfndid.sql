630d47e1298bc2b2b488704011046c34cae992ac
arodygin <arodygin@localhost>
2011-05-05 23:43:40 +1100

    Issue 283: PHP Warning: insert or update on table "tbl_reminders" violates foreign key constraint "fk_reminders_state_id"

--- src/sql/reminders/oracle/tfndid.sql
+++ src/sql/reminders/oracle/tfndid.sql
@@ -9,11 +9,13 @@ from
     tbl_projects    p,
     tbl_group_perms gp,
     tbl_membership  ms,
-    tbl_templates   t
+    tbl_templates   t,
+    tbl_states      s
 
 where
 
     p.project_id  = t.project_id   and
+    t.template_id = s.template_id  and
     t.template_id = gp.template_id and
     ms.group_id   = gp.group_id    and
     ms.account_id = a.account_id   and
